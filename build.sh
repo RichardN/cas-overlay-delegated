bf33850c122a1bf1651d563043317d8e8ca46f76
Richard Nicholas <richard.nicholas@capgemini.com>
2019-03-14 10:43:50 +0000

    copy files after doing packaging

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ function clean() {
 function package() {
     shift
 	./mvnw clean package -T 5 "$@"
-#	copy
+	copy
 }
 
 function bootrun() {
@@ -156,7 +156,6 @@ case "$1" in
 "package")
 	shift
     package "$@"
-		copy
     ;;
 "bootrun")
 	shift
